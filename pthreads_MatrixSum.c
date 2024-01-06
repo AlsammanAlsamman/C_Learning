392a6cea8f7f84d1cd7feeec54b31ceac53a297f
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-01-06 22:50:49 +0200

    Refactor matrix sum calculation using pthreads

--- pthreads_MatrixSum.c
+++ pthreads_MatrixSum.c
@@ -7,110 +7,98 @@
 //####################################################################################################
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
-#define NUM_THREADS 10
-#define NUM_ROWS 10
-#define NUM_COLS 10
-void *sum_row(void *arg);
+#define ROWN 10
+#define COLN 10
+#define THREADSN 10
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
+int rowCounter = 0;
+void *rowSumRoutine(void *argv);
 int **createMatrix();
-int **fillMatrix(int **matrix);
+int **initializeMatrix(int **matrix);
 void printMatrix(int **matrix);
+void freeMatrix(int **matrix);
 
-int rowCounter = NUM_ROWS;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-// create a rooutine to sum up the elements of a row
-
-void *sum_row(void *arg)
-{
-    int *sum = (int*)malloc(sizeof(int));
-    int *row = (int*)malloc(sizeof(int));
-    *sum = 0;
-    *row = 0;
-    
-    // get the matrix
-    int **matrix = (int **)arg;
-    pthread_mutex_lock(&mutex);
-    row = &rowCounter;
-    rowCounter--;
-    pthread_mutex_unlock(&mutex);
-    size_t i;
-    for (i = 0; i < NUM_COLS; i++)
-    {
-        *sum += matrix[*row][i];
-    }
-    printf("Sum of row %d is %d\n", *row, *sum);
-    
-    pthread_exit(NULL);
-
-
-}
 
 int **createMatrix()
 {
-    int **matrix = (int **)malloc(NUM_ROWS * sizeof(int *));
-    for (int i = 0; i < NUM_ROWS; i++)
+    int **matrix = (int **) malloc(sizeof(int*) * ROWN);
+    size_t i ;
+    for (i = 0; i < ROWN; i++)
     {
-        matrix[i] = (int *)malloc(NUM_COLS * sizeof(int));
-    }
-    return matrix;
-}
-int **fillMatrix(int **matrix)
-{
-    srand(time(NULL));
-    size_t i, j;
-    for (i = 0; i < NUM_ROWS; i++)
-    {
-        for (j = 0; j < NUM_COLS; j++)
-        {
-            matrix[i][j] = rand() % 100;
-        }
+        matrix[i] = (int *) malloc(sizeof(int) * COLN);
     }
     return matrix;
 }
 void printMatrix(int **matrix)
 {
-    size_t i, j;
-    for (i = 0; i < NUM_ROWS; i++)
+    size_t i;
+    size_t j;
+    for (i = 0; i < ROWN; i++)
     {
-        for (j = 0; j < NUM_COLS; j++)
+        for ( j = 0; j < COLN; j++)
         {
-            printf("%d\t", matrix[i][j]);
+            printf("%d\t",matrix[j][i]);
         }
         printf("\n");
     }
+    
 }
-int main(int argc , char *argv[])
+int **initializeMatrix(int **matrix)
 {
-    // create a matrix of 1000 x 1000
+    srand(time(NULL));
+    size_t i ;
+    size_t j;
+    for (i = 0; i < ROWN; i++)
+    {
+        for ( j = 0; j < COLN; j++)
+        {
+            matrix[i][j] = rand() % 2;
+        }
+    }
+    return matrix;
+}
+
+void * rowSumRoutine(void *arg)
+{
+ int *rowsum = (int*) malloc(sizeof(int));
+ int *currentRow = (int*) malloc(sizeof(int));
+ int **matrix = (int **)arg;
+
+ pthread_mutex_lock(&mutex);
+ *currentRow = rowCounter;
+ rowCounter++;
+ pthread_mutex_unlock(&mutex);
+ size_t j;
+ for (j = 0; j < COLN; j++)
+ {
+    *rowsum+=matrix[*currentRow][j];
+ }
+ printf("ROW: %d SUM:%d\n", *currentRow,*rowsum);
+}
+
+
+
+int main(int argc, char **argv)
+{
+    // create the matrix
     int **matrix = createMatrix();
-    // fill the matrix with random numbers
-    matrix = fillMatrix(matrix);
-    // print the matrix
-    printMatrix(matrix);
-    // create 5 threads
-    pthread_t threads[NUM_THREADS];
-    // create 5 threads
-    for (int i = 0; i < NUM_THREADS; i++)
+    matrix = initializeMatrix(matrix);
+    pthread_t threads[THREADSN];
+    size_t i;
+    for ( i = 0; i < THREADSN; i++)
     {
-        pthread_create(&threads[i], NULL, sum_row, (void *)matrix);
+        pthread_create(threads + i , NULL, &rowSumRoutine,(void **)matrix);
     }
-    // wait for the threads to finish
-    for (int i = 0; i < NUM_THREADS; i++)
+    for ( i = 0; i < THREADSN; i++)
     {
-        pthread_join(threads[i], NULL);
+        pthread_join(*(threads + i) , NULL);
     }
-    // destroy the mutex
-    pthread_mutex_destroy(&mutex);
-    // free the matrix
-    for (int i = 0; i < NUM_ROWS; i++)
-    {
-        free(matrix[i]);
-    }
-    free(matrix);
-    printf("Main: program completed. Exiting.\n");
+    
     return 0;
 }
