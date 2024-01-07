625b324cb3de074af494fb5ec9b392ef95984883
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-01-07 14:38:00 +0200

    Refactor code to calculate Euclidean distance and allele frequencies

--- Calculate_EuclideanForMatrix.c
+++ Calculate_EuclideanForMatrix.c
@@ -1,38 +1,55 @@
-//############################################# About Author #########################################
-// Created by: Alsamman M. Alsamman                                                                  #
-// Emails: smahmoud [at] ageri.sci.eg or A.Alsamman [at] cgiar.org or SammanMohammed [at] gmail.com  #
-// License: MIT License - https://opensource.org/licenses/MIT                                        #
-// Disclaimer: The script comes with no warranty, use at your own risk                               #
-// This script is not intended for commercial use                                                    #
-//####################################################################################################
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-// Function to calculate Euclidean distance between two samples
-double euclideanDistance(int* sample1, int* sample2, int numMarkers) {
+// Function to calculate Euclidean distance between two individuals
+double euclideanDistance(int* alleleFrequenciesX, int* alleleFrequenciesY, int numMarkers) {
     double distance = 0.0;
 
     for (int i = 0; i < numMarkers; ++i) {
-        double markerDifference = sample1[i] - sample2[i];
-        distance += markerDifference * markerDifference;
+        double frequencyDifference = alleleFrequenciesX[i] - alleleFrequenciesY[i];
+        distance += frequencyDifference * frequencyDifference;
     }
 
     return sqrt(distance);
 }
 
+// Function to calculate allele frequencies
+void calculateAlleleFrequencies(int* individual, int numMarkers, double* frequencies) {
+    // Initialize frequency counts
+    int count[3] = {0}; // 0, 1, 2 alleles
+
+    // Count occurrences of each allele
+    for (int i = 0; i < numMarkers; ++i) {
+        count[individual[i]]++;
+    }
+
+    // Calculate frequencies
+    for (int i = 0; i < 3; ++i) {
+        frequencies[i] = (double)count[i] / numMarkers;
+    }
+}
+
+
 int main() {
     // Number of markers
     int numMarkers = 10;
 
-    // Sample data (genotypes for each marker)
-    int sample1[] = {1, 0, 2, 1, 0, 2, 0, 1, 1, 2};
-    int sample2[] = {0, 1, 2, 0, 1, 1, 2, 0, 2, 1};
+    // Alleles the xth and yth individuals
+    int xthIndividual[] = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+    int ythIndividual[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+    // calculate frequencies
+    double alleleFrequenciesX[3];
+    double alleleFrequenciesY[3];
+    calculateAlleleFrequencies(xthIndividual, numMarkers, alleleFrequenciesX);
+    calculateAlleleFrequencies(ythIndividual, numMarkers, alleleFrequenciesY);
+    printf("Allele frequencies of the xth individual: %f, %f, %f\n", alleleFrequenciesX[0], alleleFrequenciesX[1], alleleFrequenciesX[2]);
+    printf("Allele frequencies of the yth individual: %f, %f, %f\n", alleleFrequenciesY[0], alleleFrequenciesY[1], alleleFrequenciesY[2]);
+    // calculate Euclidean distance
+    double distance = euclideanDistance(xthIndividual, ythIndividual, numMarkers);
+    printf("Euclidean distance between the xth and yth individuals: %f\n", distance);
 
-    // Calculate Euclidean distance
-    double distance = euclideanDistance(sample1, sample2, numMarkers);
-
-    // Print the result
-    printf("Euclidean distance between the two samples: %f\n", distance);
-
+    
+    
     return 0;
 }
