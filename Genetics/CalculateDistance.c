de3b97b0f71e8d3c18fb7736d9d646868ed2dfff
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-01-17 02:26:18 +0200

    Refactor code for improved performance and readability

---
+++ Genetics/CalculateDistance.c
@@ -0,0 +1,15 @@
+//############################################# About Author #########################################
+// Created by: Alsamman M. Alsamman                                                                  #
+// Emails: smahmoud [at] ageri.sci.eg or A.Alsamman [at] cgiar.org or SammanMohammed [at] gmail.com  #
+// License: MIT License - https://opensource.org/licenses/MIT                                        #
+// Disclaimer: The script comes with no warranty, use at your own risk                               #
+// This script is not intended for commercial use                                                    #
+//####################################################################################################
+
+// About calculating distance matrix for a set of individuals
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <pthread.h>
